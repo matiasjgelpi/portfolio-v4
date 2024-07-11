b7254cdf37e4bca0e3855777af47448e014ae3f4
matiasgelpi <matiasjgelpi@gmail.com>
2024-07-11 20:15:26 -0300

    email implementatio

--- src/components/SendMail/SendMail.tsx
+++ src/components/SendMail/SendMail.tsx
@@ -1,50 +1,73 @@
-import React, { useState } from "react";
+import React, { useRef, useState, type FormEvent } from "react";
+import emailjs from "@emailjs/browser";
 
-interface Props {
-  // Define aquí las propiedades que necesites
-}
+const SendMail: React.FC = () => {
+  const form = useRef<HTMLFormElement>(null);
+  const [sendStatus, setSendStatus] = useState<"error" | "success" | null>(
+    null
+  );
 
-interface Form {
-  name: string;
-  mail: string;
-  subject: string;
-  message: string;
-}
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    console.log(form);
+    console.log(import.meta.env);
+    setSendStatus(null);
+    e.preventDefault();
 
-const SendMail: React.FC<Props> = (props) => {
-  const [form, setForm] = useState<Form>({
-    name: "",
-    mail: "",
-    subject: "",
-    message: "",
-  });
+    const {
+      PUBLIC_EMAILJS_PUBLIC_KEY: publicKey,
+      PUBLIC_EMAILJS_SERVICE_ID: serviceId,
+      PUBLIC_EMAILJS_TEMPLATE_ID: templateId,
+    } = import.meta.env;
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+    if (form.current) {
+      emailjs
+        .sendForm(serviceId, templateId, form.current, {
+          publicKey,
+        })
+        .then(
+          () => {
+            setSendStatus("success");
+            if (form.current) form.current.reset();
+          },
+          () => {
+            setSendStatus("error");
+          }
+        );
+    }
   };
 
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setForm({
-      ...form,
-      [event.target.name]: event.target.value,
-    });
-  };
   return (
     <>
       <div className="growflex flex-col w-1/2 max-sm:w-full justify-center items-center">
+        {sendStatus === "success" && (
+          <p
+            className="animate-jump-in p-1 rounded-xl bg-green-200 text-center"
+            style={{ color: "green" }}
+          >
+            El mensaje se envío correctamente! 😊
+          </p>
+        )}
+        {sendStatus === "error" && (
+          <p
+            className="animate-jump-in p-1 rounded-xl bg-light-red text-center"
+            style={{ color: "red" }}
+          >
+            Hubo un error al enviar el mensaje. 🥲{" "}
+          </p>
+        )}
         <form
+          ref={form}
           className="flex flex-col gap-1 w-100 pr-2 text-xl font-bold"
-          onSubmit={(e) => handleSubmit(e)}
+          onSubmit={sendEmail}
         >
           <label className="flex flex-col">
             Nombre:
             <input
               className="rounded-md font-normal p-1 text-sm"
               type="text"
-              name="name"
-              onChange={(e) => handleChange(e)}
+              name="from_name"
+              id="from_name"
+              required
             />
           </label>
           <label className="flex flex-col">
@@ -52,17 +75,9 @@ const SendMail: React.FC<Props> = (props) => {
             <input
               className="rounded-md font-normal p-1 text-sm"
               type="email"
-              name="mail"
-              onChange={(e) => handleChange(e)}
-            />
-          </label>
-          <label className="flex flex-col">
-            asunto:
-            <input
-              className="rounded-md font-normal p-1 text-sm"
-              type="text"
-              name="subject"
-              onChange={(e) => handleChange(e)}
+              name="email_id"
+              id="email_id"
+              required
             />
           </label>
           <label className="flex flex-col">
@@ -70,11 +85,13 @@ const SendMail: React.FC<Props> = (props) => {
             <textarea
               className="rounded-md font-normal p-1 text-sm"
               name="message"
-              onChange={(e) => handleChange(e)}
+              id="message"
+              required
+              minLength={6}
             ></textarea>
           </label>
           <button className="bg-white rounded-md" type="submit">
-            Send
+            Enviar
           </button>
         </form>
       </div>
